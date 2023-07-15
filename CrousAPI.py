e9638681126c5d4536e540d76acf2a6f2bdcbb69
Xabi08YT <xabigoity@gmail.com>
2023-07-15 12:04:15 +0200

    Initial commit

---
+++ CrousAPI.py
@@ -0,0 +1,44 @@
+import requests as rq
+import time
+import smtplib
+import ssl
+import json
+
+
+smtp_address = 'smtp.gmail.com'
+smtp_port = 465
+
+with open(file="data.json", mode = "r") as f:
+    Data = json.loads(f)
+    f.close()
+
+
+def send_mail(content = "Message Automatique: Visionnez les appart disponibles sur le site du crous"):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as srv:
+        srv.login(Data["Sender"],Data["SenderPSW"])
+        srv.sendmail(Data["to"],content)
+
+
+while 1:
+    try:
+        r = rq.get("https://trouverunlogement.lescrous.fr/tools/31/search?maxPrice=450&occupationModes=alone&bounds=3.8070597_43.6533542_3.9413208_43.5667088")
+
+        content = r.content
+        content = content.decode()
+
+        content = content.replace("</","µ")
+        content = content.replace("&","")
+        HTMLTable = content.split("µ")
+
+        print(HTMLTable[89])
+
+        if HTMLTable[89] != 'ul>':
+            print('Appart')
+    except KeyboardInterrupt:
+        print("Arret en cours....")
+        quit(0)
+    time.sleep(15*60)
+
+
+send_mail("Test")
